129869ee7e3c68f039bfd111ab9b15ccfe24f9b3
lucasrochabz <lucasbezerrar@gmail.com>
2025-08-25 11:13:46 -0300

    ajuste na importação das imagens

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,27 +1,34 @@
-import approved from '../../../public/approved.svg';
-import failed from '../../../public/failed.svg';
 import './Result.css';
 
 export const Result = ({ isApproved }) => {
-  const content = isApproved
-    ? {
-        img: approved,
-        alt: 'Approved',
-        title: 'Adimplente',
-        message:
-          'Parabéns, seu crédito foi aprovado com sucesso. Aproveite essa nova oportunidade!',
-      }
-    : {
-        img: failed,
-        alt: 'Failed',
-        title: 'Inadimplente',
-        message:
-          'Lamentamos, mas seu crédito não foi aprovado. Você poderá tentar novamente em outra ocasião.',
-      };
+  const status = isApproved ? 'approved' : 'failed';
+
+  const images = import.meta.glob('/src/assets/*', {
+    eager: true,
+  });
+
+  const imagePath = images[`/src/assets/${status}.svg`]?.default;
+
+  const contentMap = {
+    approved: {
+      alt: 'Approved',
+      title: 'Adimplente',
+      message:
+        'Parabéns, seu crédito foi aprovado com sucesso. Aproveite essa nova oportunidade!',
+    },
+    failed: {
+      alt: 'Failed',
+      title: 'Inadimplente',
+      message:
+        'Lamentamos, mas seu crédito não foi aprovado. Você poderá tentar novamente em outra ocasião.',
+    },
+  };
+
+  const content = contentMap[status];
 
   return (
-    <div className={`result ${isApproved ? 'approved' : 'failed'}`}>
-      <img src={content.img} alt={content.alt} />
+    <div className={`result ${status}`}>
+      <img src={imagePath} alt={content.alt} />
       <h2>{content.title}</h2>
       <p>{content.message}</p>
       <a href="/">Voltar para a Home</a>
